f8311864cc6391b5678fd02d0105ad16859ba770
teshika <teshika1@gmail.com>
2017-02-12 21:38:56 -0500

    Update sketch.js
    color slider with more colors

--- Rose/sketch.js
+++ Rose/sketch.js
@@ -5,8 +5,8 @@ var sliderK;
 var sliderD;
 var sliderC;
 var counter=0;
-var cols;
 var colNum;
+var c1=1;
 
 function setup() {
   createCanvas(500,500);
@@ -15,14 +15,13 @@ function setup() {
   to = color(0, 0, 255, 0.2 * 255);
   c1 = lerpColor(from, to, .33);
   c2 = lerpColor(from, to, .66);
-  cols = [from, to, c1, c2];
 
 
   // sliderD = createSlider(1,10,1);
   // sliderK = createSlider(1,10,1);
 
   sliderC= createSlider(1,80,40);
-  sliderCol= createSlider(0,3,0);
+  sliderCol= createSlider(0,1,0,.05);
 
 
 }
@@ -32,11 +31,12 @@ function draw() {
   // d = sliderD.value();
   c = sliderC.value();
   colNum = sliderCol.value();
+  c1 = lerpColor(from, to, colNum);
   translate(width/2, height/2);
   background(255);
   beginShape();
-  stroke(cols[colNum]);
-  strokeWeight(1);
+  stroke(c1);
+  strokeWeight(1.5);
 
   for (var a=0; a< TWO_PI*10; a+=.02){
     var r = 200*cos((k/d)*a) + c;
